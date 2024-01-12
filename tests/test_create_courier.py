7ecc9876c7f2e196a6fe34afb1a14eabfe0744d3
Saya Tussipbekova <tussipbekova@gmail.com>
2024-01-12 16:09:26 +0600

    Added create courier tests

--- tests/test_create_courier.py
+++ tests/test_create_courier.py
@@ -5,55 +5,58 @@ import requests
 
 from register import register_new_courier_and_return_login_password
 
-URL = "https://qa-scooter.praktikum-services.ru"
-ENDPOINT = "/api/v1/courier"
+
+class TestGreateCourier:
+
+    URL = "https://qa-scooter.praktikum-services.ru"
+    ENDPOINT = "/api/v1/courier"
 
 
-def generate_random_string(length):
-    letters = string.ascii_lowercase
-    random_string = ''.join(random.choice(letters) for i in range(length))
-    return random_string
+    def generate_random_string(self,length):
+        letters = string.ascii_lowercase
+        random_string = ''.join(random.choice(letters) for i in range(length))
+        return random_string
 
-def test_create_courier_success():
-    # генерируем логин, пароль и имя курьера
-    login = generate_random_string(10)
-    password = generate_random_string(10)
-    first_name = generate_random_string(10)
+    def test_create_courier_success(self):
+        # генерируем логин, пароль и имя курьера
+        login = self.generate_random_string(10)
+        password = self.generate_random_string(10)
+        first_name = self.generate_random_string(10)
 
-    # собираем тело запроса
-    payload = {
-        "login": login,
-        "password": password,
-        "firstName": first_name
-    }
+        # собираем тело запроса
+        payload = {
+            "login": login,
+            "password": password,
+            "firstName": first_name
+        }
 
-    # отправляем запрос на регистрацию курьера и сохраняем ответ в переменную response
-    response = requests.post(f"{URL}{ENDPOINT}", data=payload)
+        # отправляем запрос на регистрацию курьера и сохраняем ответ в переменную response
+        response = requests.post(f"{self.URL}{self.ENDPOINT}", data=payload)
 
-    assert response.status_code == 201 and response.json() == {"ok":True}
+        assert response.status_code == 201 and response.json() == {"ok":True}
 
 
-def test_create_courier_fail():
-    login, password, first_name = register_new_courier_and_return_login_password()
-    response = requests.post(f"{URL}{ENDPOINT}", json={'login': login, 'password': password, 'first_name': first_name})
+    def test_create_courier_fail(self):
+        login, password, first_name = register_new_courier_and_return_login_password()
+        response = requests.post(f"{self.URL}{self.ENDPOINT}", json={'login': login, 'password': password, 'first_name': first_name})
 
-    assert response.status_code == 409
+        assert response.status_code == 409
 
-def test_missing_one_attribute():
-    # генерируем логин, пароль и имя курьера
-    login = generate_random_string(10)
-    password = generate_random_string(10)
-    first_name = generate_random_string(10)
+    def test_missing_one_attribute(self):
+        # генерируем логин, пароль и имя курьера
+        login = self.generate_random_string(10)
+        password = self.generate_random_string(10)
+        first_name = self.generate_random_string(10)
 
-    # собираем тело запроса
-    payload = {
-        "password": password,
-        "firstName": first_name
-    }
+        # собираем тело запроса
+        payload = {
+            "password": password,
+            "firstName": first_name
+        }
 
-    # отправляем запрос на регистрацию курьера и сохраняем ответ в переменную response
-    response = requests.post(f"{URL}{ENDPOINT}", data=payload)
+        # отправляем запрос на регистрацию курьера и сохраняем ответ в переменную response
+        response = requests.post(f"{self.URL}{self.ENDPOINT}", data=payload)
 
-    assert response.status_code == 400
+        assert response.status_code == 400
 
 
